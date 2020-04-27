f2e529aa57e4d25e7d61ec4321b28e1a819ddbc4
Brykou <11095630+Brykou@users.noreply.github.com>
2020-04-27 14:10:39 +0900

    Add minification with babel-preset-minify

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,11 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
 
-  const presets = ["@babel/preset-env", "@babel/preset-react"];
+  const presets = ["@babel/preset-env", "@babel/preset-react", "minify"];
   const plugins = [];
 
   return {
     presets,
-    plugins
+    plugins,
   };
 };
